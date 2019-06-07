530ef3741fba9dce92a3ba11520eb718e314cbc5
Teatoller <steven.otieno1@gmail.com>
2019-06-07 17:31:32 +0300

    (App): implement author quiz element
    - create function component hero to return app name and operating instruction
    - create function component turn that flicks author and books
    - create function component continue to move to next try
    - create function component to generate footer
    - create the authorquiz function that take in the components
    
    [Delivers #37]

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -1,13 +1,50 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import './bootstrap.min.css';
 
-class AuthorQuiz extends Component {
-  render() {
+function Hero() {
+  return ( <div className="row">
+    <div className="jumbotron col-10 offset-1">
+      <h1>Author Quiz</h1>
+      <p>Select the book written by the author shown</p>
+    </div>
+  </div>);
+};
+
+function Turn({author, books}) {
+  return (<div className="row turn" style={{backgroundColor: "white"}}>
+    <div className="col-4 offset-1">
+      <img src={author.imageUrl} className="authorimage" alt="Author"/>
+    </div>
+    <div className="col-6">
+      {books.map((title) => <p>{title}</p>)}
+    </div>
+  </div>);
+};
+
+function Continue() {
+  return (<div/>)
+};
+
+function Footer() {
+  return (<div id="footer" className="row">
+    <div className="jumbotron col-10 offset-1">
+      <p className="text-muted credit">All images are from <a href="https://commons.wikimedia.org/wiki/Main_Page">Wikemedia Commons</a> and are in public domain</p>
+    </div>
+  </div>)
+};
+
+function AuthorQuiz({turnData}) {
+  console.log(turnData);
     return (
-      <div>AuthorQuiz</div>
+      <div className="container-fluid">
+        <Hero/>
+        <Turn {...turnData}/>
+        <Continue/>
+        <Footer/>
+      </div> 
     );
   }
-}
+
 
 export default AuthorQuiz;
