5b36ef3f0736cb4d628d9790039ae99053b93396
Teatoller <steven.otieno1@gmail.com>
2019-06-08 09:13:22 +0300

    (index): implement random selection
    - add function to random select author
    - add method to return four set of random books
    - add method to return correct answer
    - install underscore library
    -install sample library
    
    [Delivers #005]

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import AuthorQuiz from './AuthorQuiz';
+import {shuffle, sample} from 'underscore';
 import * as serviceWorker from './serviceWorker';
 
 const authors = [
@@ -43,12 +44,25 @@ const authors = [
     }
 ];
 
-const state = {
-    turnData:{
-        author: authors[0],
-    books: authors[0].books
+function getTurnData(authors) {
+    const allBooks = authors.reduce(function (p, c, i) {
+        return p.concat(c.books);
+    }, []);
+    const fourRandomBooks = shuffle(allBooks).slice(0, 4);
+    const answer = sample(fourRandomBooks);
+
+    return {
+        books: fourRandomBooks,
+        author: authors.find((author) =>
+            author.books.some((title) =>
+                title === answer))
     }
 };
+
+const state = {
+    turnData: getTurnData(authors)
+};
+
 
 ReactDOM.render(<AuthorQuiz {...state} />, document.getElementById('root'));
 
