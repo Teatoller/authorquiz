38b4033aa89f7af664dd7ee3b1d0a66a2781407f
Teatoller <steven.otieno1@gmail.com>
2019-07-05 12:29:50 +0300

    (extract function): Implement function extract
    - extract addauthorform from the index file
    - add styling
    - add input fields and labels
    - create class component for form to introduce props
    - add onchange event
    
    [Finishes #011]

---
+++ src/AddAuthorForm.js
@@ -0,0 +1,56 @@
+import React from "react";
+import "./AddAuthorForm.css";
+
+class AuthorForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      imageUrl: ""
+    };
+    this.onFieldChange = this.onFieldChange.bind(this);
+  }
+  onFieldChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  render() {
+    return (
+      <form>
+        <div className="AddAuthorForm__input">
+          <label htmlFor="name">Name</label>
+          <br />
+          <input
+            type="text"
+            name="name"
+            value={this.state.name}
+            onChange={this.onFieldChange}
+          />
+        </div>
+        <div className="AddAuthorForm__input">
+          <label htmlFor="imgUrl">Image URL</label>
+          <br />
+          <input
+            type="text"
+            name="imageUrl"
+            value={this.state.imageUrl}
+            onChange={this.onFieldChange}
+          />
+        </div>
+      </form>
+    );
+  }
+}
+
+function AddAuthorForm({ match }) {
+  return (
+    <div className="AddAuthorForm">
+      <h1> Add Author</h1>
+      <AuthorForm />
+    </div>
+  );
+}
+
+export default AddAuthorForm;
--- src/index.js
+++ src/index.js
@@ -1,109 +1,111 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import { BrowserRouter, Route } from "react-router-dom";
-import './index.css';
-import AuthorQuiz from './AuthorQuiz';
-import {shuffle, sample} from 'underscore';
-import * as serviceWorker from './serviceWorker';
+import AddAuthorForm from './AddAuthorForm';
+import "./index.css";
+import AuthorQuiz from "./AuthorQuiz";
+import { shuffle, sample } from "underscore";
+import * as serviceWorker from "./serviceWorker";
 
 const authors = [
-    {
-        name: 'Mark Twain',
-        imageUrl: 'images/authors/marktwain.jpeg',
-        imageSource: 'Wikipedia Commons',
-        books: ['The Adventures of Huckleberry Finn', 'Life on the Mississippi', 'Roughing It']
-    },
-    {
-        name: 'Charles Dickens',
-        imageUrl: 'images/authors/charlesdickens.jpeg',
-        imageSource: 'Wikipedia Commons',
-        books: ['David Copperfield', 'A Tale of Two Cities']
-    },
-    {
-        name: 'J.K Rowling',
-        imageUrl: 'images/authors/jkrowling.jpeg',
-        imageSource: 'Wikipedia Commons',
-        books: ['Harry Porter and the Socerers Stone']
-    },
-    {
-        name: 'Joseph Conrad',
-        imageUrl: 'images/authors/josephconrad.jpeg',
-        imageSource: 'Wikipedia Commons',
-        books: ['Heart of Darkness']
-    },
-    {
-        name: 'Stephen King',
-        imageUrl: 'images/authors/stephenking.jpeg',
-        imageSource: 'Wikipedia Commons',
-        books: ['The Shinning', 'IT']
-    },
-    {
-        name: 'William Shakespeare',
-        imageUrl: 'images/authors/williamshakespear.jpeg',
-        imageSource: 'Wikipedia Commons',
-        books: ['Hamlet', 'Macbeth', 'Romeo and Juliet']
-    },
-    {
-        name: 'Chinua Achebe',
-        imageUrl: 'images/authors/chinuaachebe.jpeg',
-        imageSource: 'Wikipedia Commons',
-        books: ['Things Fall Apart', 'Arrow of God', 'No Longer at Ease', 'A  Man of the People']
-    },
-    {
-        name: 'Grace Ogot',
-        imageUrl: 'images/authors/graceogot.jpeg',
-        imageSource: 'Wikipedia Commons',
-        books: ['Land Without Thunder', 'The Other Woman', 'The Strange Bride']
-    }
+  {
+    name: "Mark Twain",
+    imageUrl: "images/authors/marktwain.jpeg",
+    imageSource: "Wikipedia Commons",
+    books: [
+      "The Adventures of Huckleberry Finn",
+      "Life on the Mississippi",
+      "Roughing It"
+    ]
+  },
+  {
+    name: "Charles Dickens",
+    imageUrl: "images/authors/charlesdickens.jpeg",
+    imageSource: "Wikipedia Commons",
+    books: ["David Copperfield", "A Tale of Two Cities"]
+  },
+  {
+    name: "J.K Rowling",
+    imageUrl: "images/authors/jkrowling.jpeg",
+    imageSource: "Wikipedia Commons",
+    books: ["Harry Porter and the Socerers Stone"]
+  },
+  {
+    name: "Joseph Conrad",
+    imageUrl: "images/authors/josephconrad.jpeg",
+    imageSource: "Wikipedia Commons",
+    books: ["Heart of Darkness"]
+  },
+  {
+    name: "Stephen King",
+    imageUrl: "images/authors/stephenking.jpeg",
+    imageSource: "Wikipedia Commons",
+    books: ["The Shinning", "IT"]
+  },
+  {
+    name: "William Shakespeare",
+    imageUrl: "images/authors/williamshakespear.jpeg",
+    imageSource: "Wikipedia Commons",
+    books: ["Hamlet", "Macbeth", "Romeo and Juliet"]
+  },
+  {
+    name: "Chinua Achebe",
+    imageUrl: "images/authors/chinuaachebe.jpeg",
+    imageSource: "Wikipedia Commons",
+    books: [
+      "Things Fall Apart",
+      "Arrow of God",
+      "No Longer at Ease",
+      "A  Man of the People"
+    ]
+  },
+  {
+    name: "Grace Ogot",
+    imageUrl: "images/authors/graceogot.jpeg",
+    imageSource: "Wikipedia Commons",
+    books: ["Land Without Thunder", "The Other Woman", "The Strange Bride"]
+  }
 ];
 
 function getTurnData(authors) {
-    const allBooks = authors.reduce(function (p, c, i) {
-        return p.concat(c.books);
-    }, []);
-    const fourRandomBooks = shuffle(allBooks).slice(0, 4);
-    const answer = sample(fourRandomBooks);
+  const allBooks = authors.reduce(function(p, c, i) {
+    return p.concat(c.books);
+  }, []);
+  const fourRandomBooks = shuffle(allBooks).slice(0, 4);
+  const answer = sample(fourRandomBooks);
 
-    return {
-        books: fourRandomBooks,
-        author: authors.find((author) =>
-            author.books.some((title) =>
-                title === answer))
-    }
-};
+  return {
+    books: fourRandomBooks,
+    author: authors.find(author => author.books.some(title => title === answer))
+  };
+}
 
 const state = {
-    turnData: getTurnData(authors),
-    highlight: ''
+  turnData: getTurnData(authors),
+  highlight: ""
 };
 
 function onAnswerSelected(answer) {
-    const isCorrect = state.turnData.author.books.some((book) => book === answer);
-    state.highlight = isCorrect ? 'correct' : 'wrong';
-    render();
-};
-
-function AddAuthorForm({match}) {
-    return <div>
-        <h1> Add Author</h1>
-        <p>{JSON.stringify(match)}</p>
-    </div>;
-};
+  const isCorrect = state.turnData.author.books.some(book => book === answer);
+  state.highlight = isCorrect ? "correct" : "wrong";
+  render();
+}
 
 function App() {
-    return <AuthorQuiz {...state} onAnswerSelected={onAnswerSelected}/>
-};
+  return <AuthorQuiz {...state} onAnswerSelected={onAnswerSelected} />;
+}
 
 function render() {
-    ReactDOM.render(
+  ReactDOM.render(
     <BrowserRouter>
-    <React.Fragment>
-    <Route exact path="/" component={App}  />
-    <Route exact path="/add" component={AddAuthorForm}  />
-    </React.Fragment>
-    
-    </BrowserRouter>, document.getElementById('root'));
-};
+      <React.Fragment>
+        <Route exact path="/" component={App} />
+        <Route exact path="/add" component={AddAuthorForm} />
+      </React.Fragment>
+    </BrowserRouter>,
+    document.getElementById("root")
+  );
+}
 render();
 
 // If you want your app to work offline and load faster, you can change
